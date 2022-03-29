d51e74701c9032b0d50dbfd90865290cf7ab535e
Tucker Steil <tuckersteil@gmail.com>
2022-03-29 13:28:41 -0400

    commit tree 5c9efdc80ee9e044d503a4af29ab2cb645944521

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'tim'
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'scott'
+}
